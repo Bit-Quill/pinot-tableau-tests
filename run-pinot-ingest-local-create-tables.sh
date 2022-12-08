448bd85617ad3e732de0ccb8d3a9f51d9b541f50
Richard Gyger <richard.gyger@improving.com>
2022-12-08 09:54:48 -0600

    Update Schemas/Ingestion specs/test data
    Update Staples schema to fix table case-sensitivity matching.
    Add pushJobSpec to ingestion specs for compatibility with latest Pinot.
    Update Calcs schema and test data to fix date/time test failures.

--- run-pinot-ingest-local-create-tables.sh
+++ run-pinot-ingest-local-create-tables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-bin/pinot-admin.sh AddTable -tableConfigFile /tmp/batters/batters-table-config.json -schemaFile /tmp/batters/batters-schema.json -exec
-bin/pinot-admin.sh LaunchDataIngestionJob -jobSpecFile /tmp/batters/batters-ingestion-spec.yml
+#bin/pinot-admin.sh AddTable -tableConfigFile /tmp/batters/batters-table-config.json -schemaFile /tmp/batters/batters-schema.json -exec
+#bin/pinot-admin.sh LaunchDataIngestionJob -jobSpecFile /tmp/batters/batters-ingestion-spec.yml
 
 bin/pinot-admin.sh AddTable -tableConfigFile /tmp/calcs/calcs-table-config.json -schemaFile /tmp/calcs/calcs-schema.json -exec
 bin/pinot-admin.sh LaunchDataIngestionJob -jobSpecFile /tmp/calcs/calcs-ingestion-spec.yml
